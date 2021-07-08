e73fa6ab4c7483b30b6893d891e95c2f05f7d086
Shruti Battu <shrutibattu@MacBookPro-16inch.local>
2021-07-08 12:15:47 +0100

    added todo class and controller

---
+++ src/main/java/com/example/demo/TodoItem.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+public class TodoItem {
+    private int id;
+    private String description;
+    private Boolean isDone;
+
+    public TodoItem(int id, String description, Boolean isDone) {
+        this.id = id;
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDone() {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+}
+
